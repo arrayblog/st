8c929b3f2af485bc707271a9e5f07788559d9674
arrayblog <815520431@qq.com>
2020-04-15 17:24:51 +0800

    shutcut

--- config.h
+++ config.h
@@ -173,7 +173,6 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
@@ -190,10 +189,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ MODKEY,				XK_k,			kscrollup,      {.i =  1} },
-	{ MODKEY,				XK_j,			kscrolldown,    {.i =  1} },
-	{ MODKEY,				XK_Page_Up,		kscrollup,      {.i = -1} },
-	{ MODKEY,				XK_Page_Down,	kscrolldown,    {.i = -1} }, 
+	{ TERMMOD,				XK_K,			kscrollup,      {.i =  1} },
+	{ TERMMOD,				XK_J,			kscrolldown,    {.i =  1} },
+	{ TERMMOD,				XK_B,			kscrollup,      {.i = -1} },
+	{ TERMMOD,				XK_F,			kscrolldown,    {.i = -1} }, 
 };
 
 /*
